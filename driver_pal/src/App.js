3bf8eaff8c50ef741251209b32427fb26779b58e
hashim <hashim2609@gmail.com>
2023-11-05 21:30:51 +0400

    new

---
+++ driver_pal/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
+function App() {
+  return (
+    <div>
+      <LoginSignUp></LoginSignUp>
+    </div>
+  );
+}
+export default App;
