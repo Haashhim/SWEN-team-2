7ba47da87a2afaef9c3e597cff4c29a8b42f14bb
cmdocmd1 <94402184+cmdocmd1@users.noreply.github.com>
2023-11-10 01:19:02 +0400

    updating fortend to communicate with backend

--- driver_pal/src/Components/LoginSignUp/LoginSignUp.jsx
+++ driver_pal/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import './LoginSignUp.css'
 import logo from '../assets/logo.png'
 import LoginBg from '../assets/loginBg.png'
 import RegisterBg from '../assets/registerBg.png'
+import AuthContext from '../../context/AuthProvidor';
+import axios from 'axios';
 <a href="https://storyset.com/user">User illustrations by Storyset</a>
 
 
 export const LoginSignUp = () => {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [email, setEmail] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
     const [isInputFocused, setInputFocused] = useState(false);
     const [isSignInActive, setSignInButtonActive] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
 
     const handleInputFocus = (e) => {
         e.target.classList.add('active');
@@ -25,13 +45,112 @@ export const LoginSignUp = () => {
         setSignInButtonActive(!isSignInActive);
         console.log(isSignInActive);
       };
-  return (
-    <main className={isSignInActive ? 'sign-up-mode' : ''}>
+
+      const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        const dataToSend = {
+            user: user,
+            pwd: pwd,
+            email: email,
+        }
+
+        await axios.post("http://localhost:8000/signup", {
+            dataToSend
+        }).then(response => {
+            if (response.data === 1)
+            {
+                alert("Sign up success");
+            }
+            else if (response.data === 0)
+            {
+                alert("username is already in use please choose another one!");
+            }
+            else
+            {
+                alert("something went wrong please try again later!");
+            }
+        }).catch(error => {
+            console.error('Error', error);
+        });
+        setUser('');
+        setPwd('');
+        setEmail('');
+      }
+
+      const handleSubmit = async (e) => {
         
+        e.preventDefault();
+
+            const dataToSend = {
+                user: user,
+                pwd: pwd,
+              };
+
+            await axios.post("http://localhost:8000/auth", {
+                dataToSend
+            }).then(response => {
+
+                if (response.data === 0)
+                {
+                    alert("Wrong username or password");
+                }
+                else if (response.data === 1)
+                {
+                    setSuccess(true);
+                }
+                else
+                {
+                    alert("Something went wrong try again later");
+                }
+              })
+              .catch(error => {
+                console.error('Error:', error);
+              });
+            
+              setUser('');
+              setPwd('');
+
+            setAuth({user, pwd})
+            //console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            //const accessToken = response?.data?.accessToken;
+            //const roles = response?.data?.roles;
+            //setAuth({ user, pwd, roles, accessToken });
+        
+        /*catch (err) {
+            if (!err?.response) {
+                setErrMsg('Server is not responding. try again later..');
+            }
+            else if (err.response?.status === 400)
+            {
+                setErrMsg('Missing username or password');
+            }
+            else if (err.response?.status === 401)
+            {
+                setErrMsg('Unauthorized!');
+            }
+            else
+            {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }*/
+      }
+  return (
+    <>
+        {success ? (
+            <section>
+                <h1>You logged In :)</h1>
+            </section>
+        ) : (
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>{errMsg}</p>
+    <main className={isSignInActive ? 'sign-up-mode' : ''}>
         <div class="box">
             <div class="inner-box">
                 <div class="forms-wrap">
-                    <form action="index.html" autocomplete="off" class="sign-in-form">
+                <form action="index.html" onSubmit={handleSubmit} autocomplete="off" class="sign-in-form">
                         <div class="logo">
                             <img src={logo} alt="easyclass"/>
                             <h4>DrivePal</h4>
@@ -43,7 +162,7 @@ export const LoginSignUp = () => {
                         </div>
                         <div class="actual-form">
                             <div class="input-wrap">
-                                <input type="text" minlength="4" autocomplete="off" 
+                                <input type="text" id="login-username" ref={userRef} minlength="4" autocomplete="off" onChange={(e) => setUser(e.target.value)} value={user} 
                                 class="input-field" 
                                 onFocus={handleInputFocus}
                                 onBlur={handleInputBlur}
@@ -51,7 +170,7 @@ export const LoginSignUp = () => {
                                 <label for="name">Name</label>
                             </div>
                             <div class="input-wrap">
-                                <input type="password" minlength="4" autocomplete="off"
+                                <input type="password" id="login-password" minlength="4" autocomplete="off" onChange={(e) => setPwd(e.target.value)} value={pwd}
                                  class="input-field" 
                                  onFocus={handleInputFocus}
                                  onBlur={handleInputBlur}
@@ -68,7 +187,7 @@ export const LoginSignUp = () => {
 
 
                     
-                    <form action="index.html" autocomplete="off" class="sign-up-form">
+                    <form action="index.html" onSubmit={handleSignUp} autocomplete="off" class="sign-up-form">
                         <div class="logo">
                             <img src={logo} alt="easyclass"/>
                             <h3>DrivePal</h3>
@@ -80,15 +199,27 @@ export const LoginSignUp = () => {
                         </div>
                         <div class="actual-form">
                             <div class="input-wrap">
-                                <input type="text" minlength="4" autocomplete="off" class="input-field" required/>
+                                <input type="text" id="username" ref={userRef} minlength="4" autocomplete="off" onChange={(e) => setUser(e.target.value)} value={user}
+                                 class="input-field" 
+                                 onFocus={handleInputFocus}
+                                 onBlur={handleInputBlur}
+                                 required/>
                                 <label for="name">Name</label>
                             </div>
                             <div class="input-wrap">
-                                <input type="email" minlength="4" autocomplete="off" class="input-field" required/>
+                                <input type="email" id="email" minlength="4" autocomplete="off" onChange={(e) => setEmail(e.target.value)} value={email}
+                                 class="input-field" 
+                                 onFocus={handleInputFocus}
+                                 onBlur={handleInputBlur}
+                                required/>
                                 <label for="name">Email</label>
                             </div>
                             <div class="input-wrap">
-                                <input type="password" minlength="4" autocomplete="off" class="input-field" required/>
+                                <input type="password" id="password" minlength="4" autocomplete="off" onChange={(e) => setPwd(e.target.value)} value={pwd}
+                                 class="input-field" 
+                                 onFocus={handleInputFocus}
+                                 onBlur={handleInputBlur}
+                                required/>
                                 <label for="password">Password</label>
                             </div>
                             <input type="submit" value="Sign Up" class="sign-btn"/>
@@ -112,5 +243,9 @@ export const LoginSignUp = () => {
             </div>
         </div>
     </main>
+    </section>
+        )}
+        </>
   )
 }
+
