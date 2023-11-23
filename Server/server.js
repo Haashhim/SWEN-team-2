acbf5cdc624a8cc0eff240c13041e21f3578efa6
cmdocmd1 <94402184+cmdocmd1@users.noreply.github.com>
2023-11-23 18:58:51 +0400

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -69,6 +69,43 @@ app.post("/signup", async(req, res) => {
 
 });
 
+app.post("/changedata", async(req, res) => {
+    const receivedData = req.body;
+    const { user, fullName, phoneNumber, location, postalCode } = receivedData.dataToSend;
+
+    const updateQuery = 'UPDATE users SET fullName = ?, phone = ?, location = ?, postalCode = ? WHERE username = ?';
+
+    connection.execute(updateQuery, [fullName, phoneNumber, location, postalCode, user], (error, results) => {
+      if (error) {
+        res.send("-1");
+      } else {
+        res.send("0");
+      }
+    });
+    
+
+});
+
+app.get("/data", async(req, res) => {
+  const username = req.query.username;
+  if (username !== "")
+  {
+    const query = 'SELECT * FROM users WHERE username = ?';
+    const [rows] = await connection.promise().execute(query, [username]);
+
+    const userData = {
+      name: rows[0].username,
+      fullName: rows[0].fullName,
+      email: rows[0].email,
+      phoneNumber: rows[0].phone,
+      location: rows[0].location,
+      postalCode: rows[0].postalCode,
+    };
+    res.json(userData);
+
+  }
+});
+
 app.listen(8000, () => {
   console.log('Server is running on http://localhost:8000');
 });
