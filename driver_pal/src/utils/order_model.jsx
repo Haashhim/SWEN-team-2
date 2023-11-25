4e19ce60c152dadc4398f0bb818010d2a69fd3ad
cmdocmd1 <94402184+cmdocmd1@users.noreply.github.com>
2023-11-25 17:28:27 +0400

    Add files via upload

---
+++ driver_pal/src/utils/order_model.jsx
@@ -0,0 +1,9 @@
+export class OrderModel {
+    
+    constructor(start_lat, start_long,end_lat,end_long) {
+      this.start_lat = start_lat;
+      this.start_long = start_long;
+      this.end_lat = end_lat;
+      this.end_long = end_long;
+    }
+  }
